2082f8acc5b83ad9559629da69b00a5d97ee0056
chhavigupta783 <chhavigupta783@gmail.com>
2025-09-14 05:04:57 -0700

    added numpy basic project with the comments

---
+++ numpy/basics/app.py
@@ -0,0 +1,29 @@
+﻿import numpy as np   # numpy import
+
+# 1D Array
+arr = np.array([1, 2, 3, 4, 5])
+print("1D Array:", arr)
+
+# 2D Array
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+print("2D Array:\n", arr2)
+
+# Zeros & Ones
+zeros = np.zeros((2,3))#(row,coloumn)(all no. will 0)
+ones = np.ones((3,2))#(row,coloumn)(all no. will be 1)
+print("Zeros:\n", zeros)
+print("Ones:\n", ones)
+
+# Random numbers
+rand_arr = np.random.randint(1, 10, size=(3,3))
+print("Random 3x3:\n", rand_arr)
+
+# Math operations
+a = np.array([10, 20, 30])
+b = np.array([1, 2, 3])
+
+print("Addition:", a + b)
+print("Multiplication:", a * b)
+print("Square Root:", np.sqrt(a))
+print("Mean:", np.mean(a))
+print("Square Root:", np.sqrt(b))
---
+++ numpy/small project matrix calculator/app.py/app.py
@@ -0,0 +1,27 @@
+﻿import numpy as np
+
+# Matrix Input
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+print("Matrix A:\n", A)
+print("Matrix B:\n", B)
+
+# Addition
+print("\nA + B:\n", A + B)
+
+# Subtraction
+print("\nA - B:\n", A - B)
+
+# Multiplication
+print("\nA x B:\n", np.dot(A, B))#dot prodduct
+
+# Transpose
+print("\nTranspose of A:\n", A.T)
+
+# Determinant
+print("\nDeterminant of A:", np.linalg.det(A))
+
+# Inverse
+print("\nInverse of A:\n", np.linalg.inv(A))
+
